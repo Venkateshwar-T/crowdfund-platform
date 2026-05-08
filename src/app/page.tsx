2a94557a46f00a36a804cfcdc99e7d472e829ee3
Venkateshwar <venkattiwari42@gmail.com>
2026-05-08 16:05:37 +0000

    Bro where is the Variety of Categories section in home page?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-
 'use client';
 
 import { HeroSection } from '@/components/hero-section';
 import { WhyChooseUs } from '@/components/why-choose-us';
+import { CategoryBubbles } from '@/components/category-bubbles';
 import { ScrollToTop } from '@/components/scroll-to-top';
 
 export default function Home() {
   return (
     <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center">
       <HeroSection />
+      <CategoryBubbles />
       <WhyChooseUs />
       <ScrollToTop />
     </main>
