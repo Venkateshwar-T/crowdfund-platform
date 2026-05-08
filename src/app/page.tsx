03f3f496426c977c823e9545e4c32ad3411284df
Venkateshwar <venkattiwari42@gmail.com>
2026-05-08 09:14:09 +0000

    Add a new section component about "Why Choose Us" explaining in simple t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import { HeroSection } from '@/components/hero-section';
 import { CategoryBubbles } from '@/components/category-bubbles';
+import { WhyChooseUs } from '@/components/why-choose-us';
 
 export default function Home() {
   return (
     <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center">
       <HeroSection />
       <CategoryBubbles />
+      <WhyChooseUs />
     </main>
   );
 }
